8140df445ccc05f91bc3e98c127534f5bd1bf8e9
AnaKaroline-23 <anakarolinesoraes.23@gmail.com>
2024-09-04 16:02:17 -0300

    atividade03

---
+++ atividade03.py
@@ -0,0 +1,5 @@
+# Crie um programa que calcule a área de um retângulo, pedindo ao usuário a largura e a altura.
+A = float(input("insira a altura "))
+L = float(input("insira a largura "))
+multiplique = (A*L)
+print(f"a multiplicaque do retângulo é = {multiplique}")
